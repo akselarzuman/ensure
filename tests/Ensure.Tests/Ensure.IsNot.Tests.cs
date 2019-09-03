f3fc41b6f5f399c15b1ec67cce583a3f38e575ac
Aksel Arzuman <akselarzuman@gmail.com>
2019-09-03 23:42:39 +0300

    * is not tests

---
+++ tests/Ensure.Tests/Ensure.IsNot.Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Chuck;
+using Xunit;
+
+namespace EnsureTests
+{
+    public class EnsureIsNotTests
+    {
+        [Fact]
+        public void Should_Throw_ArgumentNullException()
+        {
+            string val = null;
+            Assert.Throws<ArgumentNullException>(() => Ensure.IsNot<ArgumentNullException>(string.IsNullOrEmpty(val), "ArgumentNullException"));
+        }
+
+        [Fact]
+        public void Should_Not_Throw_ArgumentNullException()
+        {
+            string val = "Aksel Arzuman";
+            Ensure.IsNot<ArgumentNullException>(string.IsNullOrEmpty(val), "ArgumentNullException");
+        }
+    }
+}
